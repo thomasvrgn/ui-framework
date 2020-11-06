110f4aa274930d3b77b87e4d467c3f71910adf43
NessMC <contact@nessmc.fr>
2020-11-06 19:44:57 +0100

    ✨ Gulp sass compilation added.

--- gulpFile.js
+++ gulpFile.js
@@ -1,1 +1,14 @@
-const { series } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/**/*.scss', ['sass']);
+});
